498cbc893c6a1e2cc0e53bd41ceb5a79e23723fa
Mustafa Kemal Aktaş <mustafaktaskemal@gmail.com>
2022-04-04 18:53:36 +0300

    Temel ayarlar yapıldı ve arayüz hazırlanıyor

---
+++ application/views/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2014-2022 <a href="https://iys.mustafakemalaktas.site">Mustafa Kemal Aktaş</a>.</strong> All rights reserved.
+  </footer>
+
+  
